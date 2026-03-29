4f6b116723f891348b71e73e8c98e479945b4da9
Ehasanul Haque <ehasan.best@gmail.com>
2026-03-29 16:23:35 +0600

    Initial commit

---
+++ 01-Card-Project/src/components/card.jsx
@@ -0,0 +1,34 @@
+import { Bookmark } from 'lucide-react'
+
+const Card = (props) => {
+  
+  return (
+    <div className='card'>
+        <div>
+          <div className="top">
+          <img height={'40px'} width={'40px'} src={props.logo} alt="Logo" />
+          <button>Save <Bookmark size={12}/> </button>
+        </div>
+
+        <div className="center">
+          <h3> {props.company} <span> {props.date} </span></h3>
+          <h2> {props.post} </h2>
+          <div className='tag'>
+            <h4> {props.tag1} </h4>
+            <h4> {props.tag2} </h4>
+          </div>
+        </div>
+        </div>
+
+        <div className="bottom">
+          <div>
+            <h3> {props.salary} </h3>
+            <p> {props.office} </p>
+          </div>
+          <button>Apply Now</button>
+        </div>
+      </div>
+  )
+}
+
+export default Card
